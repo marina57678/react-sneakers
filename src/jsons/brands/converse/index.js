96761d20733ae8437fa65e310ca991e34ca9c20c
marina57678 <mshipovalova@ukr.net>
2023-03-09 07:10:50 +0300

    feat: Added styles for slider

--- src/jsons/brands/converse/index.js
+++ src/jsons/brands/converse/index.js
@@ -3,14 +3,14 @@ import converse2 from "./imgs/converse2.svg";
 import converse3 from "./imgs/converse3.svg";
 import converse4 from "./imgs/converse4.svg";
 import converse5 from "./imgs/converse5.svg";
-// import converse6 from "./imgs/converse6.svg";
+import converse6 from "./imgs/converse6.svg";
 // import converse3 from "/img/converse/converse3.svg";
 // import converse4 from "/img/converse/converse4.svg";
 // import converse5 from "/img/converse/converse5.svg";
 
 export const listOfConverse = {
    name: "Converse",
-   pictures: [converse1, converse2, converse3, converse4, converse5],
+   pictures: [converse1, converse2, converse3, converse4, converse5, converse6],
    slogan: "GOOD SHOES TAKE YOU GOOD PLACES",
 };
 
